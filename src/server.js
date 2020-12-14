41cda16b18711127d291029d96b2b1f5c8d6b72c
DmitriyFriz <dmitriy.bulavko.friz44@gmail.com>
2020-12-14 23:16:28 +0300

    fix: add handle exception and rejection for app

--- src/server.js
+++ src/server.js
@@ -1,25 +1,26 @@
 const { PORT } = require('./common/config');
-const app = require('./app');
 const winston = require('./common/logger/winston');
+
+const errorTransport = winston.transports[1];
+
+process
+  .on('uncaughtException', () => {
+    errorTransport.on('open', () => {
+      // wait until errorTransport._dest is ready
+
+      errorTransport._dest.on('finish', () => {
+        // eslint-disable-next-line no-process-exit
+        process.exit(1);
+      });
+      winston.end();
+    });
+  })
+  .on('unhandledRejection', reason => {
+    if (reason instanceof Error) throw reason;
+  });
+
+const app = require('./app');
 
 app.listen(PORT, () =>
   console.log(`App is running on http://localhost:${PORT}`)
 );
-
-const errorTransport = winston.transports[1];
-
-process.on('uncaughtException', () => {
-  errorTransport.on('open', () => {
-    // wait until errorTransport._dest is ready
-
-    errorTransport._dest.on('finish', () => {
-      // eslint-disable-next-line no-process-exit
-      process.exit(1);
-    });
-    winston.end();
-  });
-});
-
-process.on('unhandledRejection', reason => {
-  if (reason instanceof Error) throw reason;
-});
